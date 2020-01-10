c46b6feae1347e879d635d14a5e8f28dbbe431db
Can KAYA <cankaya07@gmail.com>
2020-01-11 00:45:17 +0300

    easily to share winizleyici

---
+++ InstallUninstall.ps1
@@ -0,0 +1,60 @@
+$url="http://35.156.50.195/esbackup/winizleyici.zip"
+$file ="winizleyici.zip"
+$output = $env:APPDATA+"\Aryasoft"
+$destFile = $output+"\winizleyici.zip"
+
+Set-ExecutionPolicy Bypass -Force
+
+
+function Install(){
+    New-Item -ItemType Directory -Force -Path $output
+    Download
+    while(test-path $destFile)
+    {
+        Expand-Archive $destFile -DestinationPath $output
+        rm $destFile
+        break
+    }
+
+    cd $output
+    .\winizleyici\install-service-filebeat.ps1
+    Write-Host -BackgroundColor Green -ForegroundColor White "******************"
+    Write-Host -BackgroundColor Green -ForegroundColor White "Install Completed"
+    Write-Host -BackgroundColor Green -ForegroundColor White "******************"
+}
+
+function Download(){
+    (New-Object System.Net.WebClient).DownloadFile($url, $destFile)
+    Write-Host -BackgroundColor Green -ForegroundColor White "Download Completed"
+}
+
+function StartP(){
+     cd $output
+    .\winizleyici\saruman.exe
+    #Start-Service filebeat
+    Write-Host -BackgroundColor Green -ForegroundColor White "Started"
+}
+
+function Stop(){
+    kill -Name saruman
+    Stop-Service filebeat
+    Write-Host -BackgroundColor Green -ForegroundColor Red "Stopped"
+}
+
+
+function Uninstall(){
+    cd $output
+    .\winizleyici\uninstall-service-filebeat.ps1 
+    cd ..
+    rm Aryasoft -Force -Recurse
+    Write-Host -BackgroundColor Green -ForegroundColor Red "Uninstall Completed"
+}
+
+
+Install
+StartP
+
+
+
+#Stop
+#Uninstall
