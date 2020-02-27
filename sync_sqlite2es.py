f5c5e432559ee93aa4dc7da73833dcb19d96d667
kyilmaz80 <kyilmaz80@gmail.com>
2020-02-27 23:46:55 +0300

    bulk insert ok
    bulk insert eklendş am temizlik gerekiyor.

--- sync_sqlite2es.py
+++ sync_sqlite2es.py
@@ -2,7 +2,9 @@ import sqlite3
 from sqlite3 import Error
 import json
 from elasticsearch import Elasticsearch
+from elasticsearch.helpers import bulk, streaming_bulk
 from collections import OrderedDict
+import uuid
 
 def create_connection(db_file):
     """ create a database connection to the SQLite database
@@ -57,13 +59,16 @@ def rows_to_dict(rows_list):
     assert len(rows_list) != 0
     # rows bir liste
     # icinde de tupple var
-    keys = ["id", "process", "title", "username", "start_date", "end_date", "duration"]
+    keys = ["processname", "windowtitle", "username",
+            "createdate", "enddate", "durationmin"]
     for row_tupple in rows_list:
         #print("row_tupple: ", row_tupple)
         data_dict = {}
         for i in range(len(row_tupple)):
             data_dict[keys[i]] = row_tupple[i]
         data_rows.append(data_dict)
+    if len(rows_list) > 1:
+        data_rows.pop() # last row siliniyor...
 
     return data_rows
 
@@ -75,6 +80,34 @@ def dick_to_json(rows_dict):
         print(e)
         return None
     return json_data
+"""
+def get_bulk_dict(rows_list, index_name, doc_type):
+    # source u olusturma
+    source = ""
+    actions = []
+    for rows_dict in rows_list:
+        for k, v in rows_dict.items():
+            source += '"' + k + '":"' + str(v) + '",'
+        action = {
+            "_index": index_name,
+            "_type": doc_type,
+            "_source": "{" + source +
+                        '"id":' +  str(uuid.uuid4()) + "}"
+            }
+        if action != "":
+            actions.append(action)
+    print("actions = ", actions)
+    return actions
+"""
+
+def gen_data(data_rows):
+    for data in data_rows:
+        yield {
+            "id": str(uuid.uuid4()),
+            "_index": "test",
+            "_type": "_doc",
+            "_source": data,
+        } 
 
 def connect_to_elastic(host, port, username, password):
     es = Elasticsearch(host=host, port=port, http_auth=(username, password),)
@@ -83,31 +116,39 @@ def connect_to_elastic(host, port, username, password):
     else:
         return None
 
-def insert_to_elasticsearch(es, index_name, data_json):
+def insert_to_elasticsearch(es, index_name, data_json, doc_type):
     #es  = connect_to_elastic(host, port, username, password)
     res = {}
     res["result"] = ""
     if es.ping():
         try:
             #TODO: mapper hatası mapper_parsing_exception
-            res = es.index(index_name, body=data_json)
+            print(data_json)
+            res = es.index(index_name, body=data_json,
+                           doc_type=doc_type)
         except Exception as e:
             print(e)
         return res["result"] == "created"
     else:
         return None
-    
+
+def add_bulk_es(es, data_json):
+    print(data_json)
+    res = bulk(es, data_json)
+    return res
+
 def main():
     database = r"C:\Users\Koray\Documents\AutoIT_win_capture\saruman_branch\worklog.db"
 
     #sqlite parameters
-    MAX_RECORD_LEN = 2
+    MAX_RECORD_LEN = 10
     #elasticsearch parameters
     HOST = "elasticsearch.aryasoft.com.tr"
     USERNAME = "elasticsearch"
     PASSWORD = "SecureElast1c"
     PORT = "8080"
     INDEX_NAME = "test"
+    DOCUMENT_TYPE = "_doc"
     
     # create a database connection
     conn = create_connection(database)
@@ -119,29 +160,30 @@ def main():
 
     #elasticsearch  connect & insert
     data_dict = rows_to_dict(data)
-    data_json = dick_to_json(data_dict)
+    data_json = dick_to_json(data_dict[0])
+    data_json.replace("'", "\"")
     es  = connect_to_elastic(HOST, PORT, USERNAME, PASSWORD)
-    delete_elastic_index(es, INDEX_NAME)
+    #delete_elastic_index(es, INDEX_NAME)
 
     if es:
-        if insert_to_elasticsearch(es, INDEX_NAME, data_json):
+        """
+        if insert_to_elasticsearch(es, INDEX_NAME, data_json, DOCUMENT_TYPE):
             print("data_dict[0] :", data_dict[0])
             print("data_dict[-1] :", data_dict[-1])
+        """
+        try:
+            res = bulk(es, gen_data(data_dict))
+        except Exception as e:
+            print(e)
+
+        print("result = ", res)
+        
+        
+        #get_bulk_dict(data_dict, INDEX_NAME, DOCUMENT_TYPE)
+        #if add_bulk_es(es, bulk_str):
+        #    print("bulk eklendi dostum")
     else:
         print("Insert edilemedi!")
-    
-    """
-    data_dict = rows_to_dict(data)
-    data_json = dick_to_json(data_dict)
-    #print("JSON:")
-    #print(data_json)
-    print("......")
-    print("data_dict[0] :", data_dict[0])
-    print(type(data_dict), len(data_dict))
-    print("....")
-    print("data_dict[-1] : ", data_dict[-1])
-    """
- 
  
 if __name__ == '__main__':
     main()
